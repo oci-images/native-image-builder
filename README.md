665483b9739adeb7f9df6f41416fd22a79556c2f
Demonio Azteka <rickyrg@gmail.com>
2021-07-04 19:58:06 -0500

    init repos

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# native-image-builder
+# OCI Image with GraalVM and Native Image
+Custom Images for Builder Native Image for Tekton Task Process
+
+Image build with default:
+
+WORKDIR ***/workspace/target***
+
+ENTRYPOINT _/bin/bash_
+
+CMD _native-image-compiler.sh_
+
+
+|Environment Vars|Description|
+|---|---|
+|NATIVE_OPTIONS| set adicional native image options for build: _--enable-https_|
+|NATIVE_RESOURCES| set the ResourceConfigurationFiles if uses resources set yours files _resources-config.json_|
+|NATIVE_REFLECTION| set the ResourceConfigurationFiles if uses reflection set yours files _reflection-config.json_|
+|JAR_APP| if copy multiples jar files, you must be set a jar with mainclass|
+
+
+### Docker run
+```
+docker pull demonioazteka/native-image-builder:latest
+
+docker run --env NATIVE_RESOURCES="reflection-config.json" -v MY_JAVA_BUILD_DIR:/workspace/target demonioazteka/native-image-builder 
+```
---
+++ native-image-compiler.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+echo "Graalvm Native Image Compiler"
+
+export NI_OPTIONS="--no-server --no-fallback --allow-incomplete-classpath"
+if [ ! -z "$NATIVE_OPTIONS" ]
+then	
+	export NI_OPTIONS="$NI_OPTIONS $NATIVE_OPTIONS"
+fi
+
+if [ ! -z "$NATIVE_RESOURCES" ]
+then	
+	export NI_RESOURCES=""
+else	
+	export NI_RESOURCES="-H:ResourceConfigurationFiles=$NATIVE_RESOURCES"
+fi
+
+if [ ! -z "$NATIVE_REFLECTION" ]
+then	
+	export NI_REFLECTION=""
+else	
+	export NI_REFLECTION="-H:ReflectionConfigurationFiles=$NATIVE_REFLECTION"
+fi
+
+filesnum=$(ls *.jar | wc -l)
+if [ $filesnum = 1 ]; 
+then
+	jarfile=$(ls *.jar)
+	echo "INFO exec java $JRE_OPTION $JRE_CP -jar $JAR_APP $JRE_ARGS"
+	native-image $NI_OPTIONS $NI_REFLECTION $NI_RESOURCES -jar $jarfile
+else
+	if [ -z "$JAR_APP" ]
+	then	
+		echo "Jar files: ${filesnum}, you must be set JAR_APP"
+	else
+		echo "INFO exec java $JRE_OPTION $JRE_CP -jar $JAR_APP $JRE_ARGS"
+		native-image $NI_OPTIONS $NI_REFLECTION $NI_RESOURCES -jar $jarfile
+	fi	
+
+fi
